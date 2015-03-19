0ee99e721d34872e457c8e68fc28fa4f7e9556e0
BoringCode <rosenfeld.bradley@gmail.com>
2015-03-19 15:51:50 -0400

    Changed lasertag db module

---
+++ server/lasertag-db/lasertag-db.js
@@ -0,0 +1,6 @@
+var laserTag = exports,
+    Mysql = require('mysql');
+
+laserTag.test = function test(test) {
+  console.log("test");
+}
--- server/socket/server.js
+++ server/socket/server.js
@@ -7,7 +7,7 @@
 
  var config = require('../config.json')[process.env.NODE_ENV || 'dev'];
  var ip = require("ip");
- var db = require("lasertag-db");
+ var db = require("../lasertag-db/lasertag-db.js");
  var net = require("net");
 
 
