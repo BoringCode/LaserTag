4f28462e839251bb83e14784644c714603b48a94
BoringCode <rosenfeld.bradley@gmail.com>
2015-03-19 15:26:10 -0400

    Setting up server design

--- server/socket/server.js
+++ server/socket/server.js
@@ -1,38 +1,43 @@
 /*
  * Laser Tag Server
  * 2015 Bradley Rosenfeld, Micah Russell, Dustin Waldron
- * 
+ *
  * Simple socket server for communicating with guns
  */
 
-var ip = require("ip");
-var net = require("net");
+ var config = require('../config.json')[process.env.NODE_ENV || 'dev'];
+ var ip = require("ip");
+ var db = require("lasertag-db");
+ var net = require("net");
 
-var HOST = ip.address();
-var PORT = 8888;
+
+ var HOST = config.server.host || ip.address();
+ var PORT = config.server.socketPort || 8888;
 
 // Create a server instance, and chain the listen function to it
 net.createServer(function(sock) {
-    
+
 	// We have a connection - a socket object is assigned to the connection automatically
 	console.log('LASER TAG CLIENT CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-	
+
 	//Send stuff to the client
 	sock.write("HEY THERE");
-    
+
 	// Add a 'data' event handler to this instance of socket
 	sock.on('data', function(data) {
         	//Print what was sent by the client
-		console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        	console.log('DATA ' + sock.remoteAddress + ': ' + data);
         });
-    
+
 	// Add a 'close' event handler to this instance of socket
 	sock.on('close', function(data) {
 		//Display a message on close
 		console.log('LASER TAG CLIENT CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
 	});
-    
+
 }).listen(PORT, HOST);
 
 //Tell everyone where I'm running
 console.log('Laser Tag server listening on ' + HOST +':'+ PORT);
+
+module.exports = net;
---
+++ server/web/server.js
@@ -0,0 +1,94 @@
+/**
+ * Runs server to host express app
+ */
+
+var config = require('../config.json')[process.env.NODE_ENV || 'dev'];
+var app = require('./app');
+var ip = require("ip");
+var debug = require('debug')('web:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(config.server.webPort || '3000');
+var host = config.server.host || ip.address();
+app.set('port', port);
+app.set('host', host)
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port, host);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = app;
